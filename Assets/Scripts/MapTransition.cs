44d339701e7a26a67072a43e0d7b68a539fc9567
chiyanli <richardlcy0907@gmail.com>
2025-10-29 13:28:29 -0400

    map transition function completed

---
+++ Assets/Scripts/MapTransition.cs
@@ -0,0 +1,50 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundry;
+    CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos = 3f;
+
+    enum Direction { Up, Down, Left, Right }
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch(direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x += additivePos;
+                break;
+            case Direction.Right:
+                newPos.x -= additivePos;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+}
