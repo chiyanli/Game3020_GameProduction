d9b296280aba290e3a51ac69aa8b99a7282bd7b5
chiyanli <richardlcy0907@gmail.com>
2025-10-30 08:05:33 -0400

    audio effect, background music

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,6 +8,10 @@ public class Item : MonoBehaviour
     public int ID;
     public string Name;
 
+    [Header("SFX")]
+    public AudioClip pickupSfx;
+    [Range(0f, 1f)] public float sfxVolume = 1f;
+
     public virtual void UseItem()
     {
         Debug.Log("Using item " + Name);
@@ -15,6 +19,12 @@ public class Item : MonoBehaviour
 
     public virtual void PickUp()
     {
+        if (pickupSfx != null)
+        {
+            var cam = Camera.main != null ? Camera.main.transform.position : transform.position;
+            AudioSource.PlayClipAtPoint(pickupSfx, cam, sfxVolume);
+        }
+
         Sprite itemIcon = GetComponent<Image>().sprite;
         if (ItemPickupUIController.Instance != null)
         {
